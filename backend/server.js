6eded425ddf123e74ca6b4ea37f129d56455f434
hammadali03 <ham223006@gmail.com>
2024-08-28 00:12:34 +0500

    new

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ const app=express()
 const __dirname = path.resolve();
 // const port=process.env.PORT||4000
 
-app.use(cors({ origin: ["https://full-auth-back.vercel.app/"],methods:["GET","POST","PUT","DELETE"], credentials: true }));
+// app.use(cors({ origin: ["https://full-auth-back.vercel.app/"],methods:["GET","POST","PUT","DELETE"], credentials: true }));
+app.use(cors());
 
 //middleware
 
