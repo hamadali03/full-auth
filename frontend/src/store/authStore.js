b2daf84fc170bf6cf57e4f9726034f7125930fcc
hammadali03 <ham223006@gmail.com>
2024-08-28 15:46:53 +0500

    front

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,8 +2,9 @@ import { create } from "zustand";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-const API_URL = import.meta.env.MODE === "development" ?"http://localhost:5000/api/auth"
-    // ? "https://full-auth-backend.vercel.app/api/auth" 
+const API_URL = import.meta.env.MODE === "development" ?   "https://full-auth-backend.vercel.app/api/auth" 
+// "http://localhost:5000/api/auth"
+  
     : "/api/auth";
 
 // "https://full-auth-back.vercel.app/api/auth" 
