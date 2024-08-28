047f4ecfbb15f243f4a85b800c539bc74bb992f1
hammadali03 <ham223006@gmail.com>
2024-08-28 23:35:33 +0500

    add some lines

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,15 +1,13 @@
 import { create } from "zustand";
 import axios from "axios";
 
+
+const API_URL = 'https://full-auth-gold.vercel.app/api/auth'
+// "http://localhost:5000/api/auth" 
+// import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "https://full-auth-backend.vercel.app/api/auth";
+
+
 axios.defaults.withCredentials = true;
-const API_URL = import.meta.env.MODE === "development" ?   "https://full-auth-backend.vercel.app/api/auth" 
-// "http://localhost:5000/api/auth"
-  
-    : "/api/auth";
-
-// "https://full-auth-back.vercel.app/api/auth" 
-
-  
 
 
 export const useAuthStore = create((set) => ({
